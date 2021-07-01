481f9d28a4cd52f1616c58184ef3a9e2a7f39aed
Kyoungmin Kim <kyounkim@c5r3s1.42seoul.kr>
2021-07-01 19:56:39 +0900

    get_value_in_stacke 구현중

--- push_swap/ps_srcs/get_value_in_stack.c
+++ push_swap/ps_srcs/get_value_in_stack.c
@@ -6,9 +6,36 @@
 /*   By: kyounkim <kyounkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 17:50:14 by kyounkim          #+#    #+#             */
-/*   Updated: 2021/06/29 17:50:30 by kyounkim         ###   ########.fr       */
+/*   Updated: 2021/07/01 19:55:59 by kyounkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+int	get_max_value(t_stack **head, int stack_size)
+{
+	int *sort;
+	int max_value;
+
+	sort = get_stack_array(head, stack_size);
+	quick_sort(sort, 0, stack_size - 1);
+	max_value = sort(stack_size - 1);
+	free(sort);
+}
+
+int	get_value_index(t_stack **head, int value)
+{
+	int	index;
+	t_stack *node;
+
+	index = 0;
+	node = (*head);
+	while (node != NULL)
+	{
+		if (node->n == value)
+			return (index);
+		node = node->next;
+		index++;
+	}
+	return (index);
+}
