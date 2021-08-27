e346887fb6c42e1d627078b775b8aca3ecd08361
kyoungmin <rtong77@naver.com>
2021-08-27 17:50:50 +0900

    sort_main 함수 수정

--- push_swap/ps_srcs/sort_main_a.c
+++ push_swap/ps_srcs/sort_main_a.c
@@ -42,15 +42,15 @@ void	sort_main_a(t_stack **a, t_stack **b, t_op **op, int stack_size)
 		{
 			get_op_list(op, "pb", a, b);
 			if ((*b)->n >= get_low_pivot(a, stack_size) && ++count.count_rb)
-				get_op_list(op, "rb", a, b);
+				get_op_list(op, "rb", NULL, b);
 		}
 		else if ((*a)->n >= get_high_pivot(a, stack_size) && ++count.count_ra)
-			get_op_list(op, "ra", a, b);
+			get_op_list(op, "ra", a, NULL);
 	}
 	exe_rrr(a, b, op, count.count_ra);
 	if (count.count_ra > count.count_rb)
-		get_op_list(op, "rb", a, b);
-	sort_main_a(a, b, op, stack_size - count.push);
+		get_op_list(op, "rb", NULL, b);
+	sort_main_a(a, b, op, count.count_ra);
 	sort_main_b(a, b, op, count.count_rb);
 	sort_main_b(a, b, op, count.push - count.count_rb);
 }
--- push_swap/ps_srcs/sort_main_b.c
+++ push_swap/ps_srcs/sort_main_b.c
@@ -51,15 +51,15 @@ void	sort_main_b(t_stack **a, t_stack **b, t_op **op, int stack_size)
 		{
 			get_op_list(op, "pa", a, b);
 			if ((*a)->n < get_high_pivot(b, stack_size) && ++count.count_ra)
-				get_op_list(op, "ra", a, b);
+				get_op_list(op, "ra", a, NULL);
 		}
 		else if ((*b)->n < get_low_pivot(b, stack_size) && ++count.count_rb)
-			get_op_list(op, "rb", a, b);
+			get_op_list(op, "rb", NULL, b);
 	}
 	sort_main_a(a, b, op, count.push - count.count_ra);
 	exe_rrr(a, b, op, count.count_ra);
 	if (count.count_ra > count.count_rb)
-		get_op_list(op, "rb", a, b);
+		get_op_list(op, "rb", NULL, b);
 	sort_main_a(a, b, op, count.count_ra);
-	sort_main_b(a, b, op, stack_size - count.push);
+	sort_main_b(a, b, op, count.count_rb);
 }
