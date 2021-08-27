6e5121b383f7a779aa397be01db59176b5fed4ec
kyoungmin <rtong77@naver.com>
2021-08-28 01:03:26 +0900

    gnl 수정

--- push_swap/common_srcs/ft_strdup.c
+++ push_swap/common_srcs/ft_strdup.c
@@ -21,10 +21,10 @@ char	*ft_strdup(char *s)
 	if (!s)
 		return (0);
 	i = 0;
+	s_len = ft_strlen(s);
 	result = (char *)malloc(sizeof(char) * (s_len + 1));
 	if (!result)
 		return (0);
-	s_len = ft_strlen(s);
 	while (i < s_len)
 	{
 		result[i] = s[i];
